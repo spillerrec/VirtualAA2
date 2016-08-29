b1093432c5de40d237c66033aa50a05b4bc1031f
spillerrec <spillerrec@hotmail.com>
2016-08-29 21:55:10 +0200

    FIX: Catch exception before they crashes dokan and BSOD your system ;)

--- src/filesystem-win.cpp
+++ src/filesystem-win.cpp
@@ -79,147 +79,182 @@ static VirtualDataDir& getRoot( PDOKAN_FILE_INFO file_info )
 namespace VirtualAA2{
 
 NTSTATUS DOKAN_CALLBACK CreateFile( LPCWSTR filename, PDOKAN_IO_SECURITY_CONTEXT, ACCESS_MASK DesiredAccess, ULONG FileAttributes, ULONG ShareAccess, ULONG CreateDisposition, ULONG CreateOptions, PDOKAN_FILE_INFO file_info ){
-	bool read_only = false;
-	auto dir = getRoot( file_info ).getFromPath( { filename } );
-	if( !dir )
-		return STATUS_OBJECT_NAME_NOT_FOUND;
-	
-//A long range of access checks
-	/*
-	if( DesiredAccess & (FILE_READ_EA | FILE_EXECUTE) ){
-		std::wcout << "Unknown access wanted for: " << filename << "\n";
-		return STATUS_NOT_IMPLEMENTED;
-	}
-	*/
-	
-	if( DesiredAccess & (FILE_WRITE_DATA | FILE_WRITE_ATTRIBUTES) && (!dir->canWrite() || read_only) ){
-		std::wcout << "Write not allowed for: " << filename << "\n";
-		return STATUS_ACCESS_VIOLATION;
-	}
-	
-	/*
-	if( DesiredAccess & (FILE_WRITE_EA | FILE_WRITE_ATTRIBUTES) ){
-		std::wcout << "Attribute changes not implemented: " << filename;
-		return STATUS_NOT_IMPLEMENTED;
-	}
-	*/
-	
-	file_info->IsDirectory = dir->isDir();
-	
-	if( !dir->isDir() ){
-		const char* debug = "";
-		if( DesiredAccess & FILE_READ_DATA ){
-			debug = "Creating read handle";
-			handleToContext( file_info, PersistentFileObject::createReadAccess( *dir ) );
-		}
-		else if( DesiredAccess & FILE_WRITE_DATA ){
-			debug = "Creating write handle";
-			handleToContext( file_info, PersistentFileObject::createWriteAccess( *dir ) );
-		}
-		else if( DesiredAccess & FILE_APPEND_DATA ){
-			debug = "Creating append handle";
-			handleToContext( file_info, PersistentFileObject::createAppendAccess( *dir ) );
-		}
-		else if( DesiredAccess & FILE_READ_ATTRIBUTES )
-			debug = "Not understood read attr";
-		else if( DesiredAccess & FILE_WRITE_ATTRIBUTES )
-			debug = "Not understood write attr";
-		else
-			debug = "Unknown access request";
+	try{
+		bool read_only = false;
+		auto dir = getRoot( file_info ).getFromPath( { filename } );
+		if( !dir )
+			return STATUS_OBJECT_NAME_NOT_FOUND;
 		
-	//	std::wcout << debug << " for: " << filename << "\n";
+	//A long range of access checks
+		/*
+		if( DesiredAccess & (FILE_READ_EA | FILE_EXECUTE) ){
+			std::wcout << "Unknown access wanted for: " << filename << "\n";
+			return STATUS_NOT_IMPLEMENTED;
+		}
+		*/
+		
+		if( DesiredAccess & (FILE_WRITE_DATA | FILE_WRITE_ATTRIBUTES) && (!dir->canWrite() || read_only) ){
+			std::wcout << "Write not allowed for: " << filename << "\n";
+			return STATUS_ACCESS_VIOLATION;
+		}
+		
+		/*
+		if( DesiredAccess & (FILE_WRITE_EA | FILE_WRITE_ATTRIBUTES) ){
+			std::wcout << "Attribute changes not implemented: " << filename;
+			return STATUS_NOT_IMPLEMENTED;
+		}
+		*/
+		
+		file_info->IsDirectory = dir->isDir();
+		
+		if( !dir->isDir() ){
+			const char* debug = "";
+			if( DesiredAccess & FILE_READ_DATA ){
+				debug = "Creating read handle";
+				handleToContext( file_info, PersistentFileObject::createReadAccess( *dir ) );
+			}
+			else if( DesiredAccess & FILE_WRITE_DATA ){
+				debug = "Creating write handle";
+				handleToContext( file_info, PersistentFileObject::createWriteAccess( *dir ) );
+			}
+			else if( DesiredAccess & FILE_APPEND_DATA ){
+				debug = "Creating append handle";
+				handleToContext( file_info, PersistentFileObject::createAppendAccess( *dir ) );
+			}
+			else if( DesiredAccess & FILE_READ_ATTRIBUTES )
+				debug = "Not understood read attr";
+			else if( DesiredAccess & FILE_WRITE_ATTRIBUTES )
+				debug = "Not understood write attr";
+			else
+				debug = "Unknown access request";
+			
+		//	std::wcout << debug << " for: " << filename << "\n";
+		}
+		
+		return STATUS_SUCCESS;
 	}
-	
-	return STATUS_SUCCESS;
+	catch( std::exception& e ){
+		std::cout << e.what() << std::endl;
+		return STATUS_UNSUCCESSFUL;
+	}
 }
 
 
 NTSTATUS DOKAN_CALLBACK ReadFile( LPCWSTR filename, LPVOID buffer, DWORD bytes_to_read, LPDWORD bytes_read, LONGLONG offset, PDOKAN_FILE_INFO file_info ){
-	auto file = contextToHandle( file_info );
-	if( !file ){
-		std::cout << "Read handle was missing, re-creating it\n";
-		auto dir = getRoot( file_info ).getFromPath( { filename } );
-		if( !dir )
-			return STATUS_OBJECT_NAME_NOT_FOUND;
-		file = PersistentFileObject::createReadAccess( *dir );
+	try{
+		auto file = contextToHandle( file_info );
+		if( !file ){
+			std::cout << "Read handle was missing, re-creating it\n";
+			auto dir = getRoot( file_info ).getFromPath( { filename } );
+			if( !dir )
+				return STATUS_OBJECT_NAME_NOT_FOUND;
+			file = PersistentFileObject::createReadAccess( *dir );
+		}
+		
+		ByteView read_buffer( static_cast<uint8_t*>(buffer), bytes_to_read );
+		*bytes_read = file->handle->read( read_buffer, offset );
+	//	std::wcout << "ReadFile: " << filename << " - " << bytes_to_read << "\n";
+		return STATUS_SUCCESS;
 	}
-	
-	ByteView read_buffer( static_cast<uint8_t*>(buffer), bytes_to_read );
-	*bytes_read = file->handle->read( read_buffer, offset );
-//	std::wcout << "ReadFile: " << filename << " - " << bytes_to_read << "\n";
-	return STATUS_SUCCESS;
+	catch( std::exception& e ){
+		std::cout << e.what() << std::endl;
+		return STATUS_UNSUCCESSFUL;
+	}
 }
 
 NTSTATUS DOKAN_CALLBACK WriteFile( LPCWSTR filename, LPCVOID buffer, DWORD bytes_to_write, LPDWORD bytes_written, LONGLONG offset, PDOKAN_FILE_INFO file_info ){
-	PersistentFileObject* file = contextToHandle( file_info );
-	if( !file ){
-		std::cout << "Write handle was missing, re-creating it\n";
-		auto dir = getRoot( file_info ).getFromPath( { filename } );
-		if( !dir )
-			return STATUS_OBJECT_NAME_NOT_FOUND;
-		file = PersistentFileObject::createWriteAccess( *dir );
+	try{
+		PersistentFileObject* file = contextToHandle( file_info );
+		if( !file ){
+			std::cout << "Write handle was missing, re-creating it\n";
+			auto dir = getRoot( file_info ).getFromPath( { filename } );
+			if( !dir )
+				return STATUS_OBJECT_NAME_NOT_FOUND;
+			file = PersistentFileObject::createWriteAccess( *dir );
+		}
+		
+		ConstByteView write_buffer( static_cast<const uint8_t*>(buffer), bytes_to_write );
+		*bytes_written = file->handle->write( write_buffer, offset );
+		std::wcout << "WriteFile: " << filename << " - " << *bytes_written << "\n";
+		return STATUS_SUCCESS;
 	}
-	
-	ConstByteView write_buffer( static_cast<const uint8_t*>(buffer), bytes_to_write );
-	*bytes_written = file->handle->write( write_buffer, offset );
-	std::wcout << "WriteFile: " << filename << " - " << *bytes_written << "\n";
-	return STATUS_SUCCESS;
+	catch( std::exception& e ){
+		std::cout << e.what() << std::endl;
+		return STATUS_UNSUCCESSFUL;
+	}
 }
 
 
 NTSTATUS DOKAN_CALLBACK GetFileInformation( LPCWSTR filename, LPBY_HANDLE_FILE_INFORMATION info, PDOKAN_FILE_INFO file_info ){
-	auto dir = getRoot( file_info ).getFromPath( { filename } );
-	if( !dir )
-		return STATUS_OBJECT_NAME_NOT_FOUND;
-	
-	info->dwFileAttributes = dir->isDir() ? FILE_ATTRIBUTE_DIRECTORY : FILE_ATTRIBUTE_NORMAL;
-	
-	info->ftCreationTime   = toFileTime( dir->created() );
-	info->ftLastAccessTime = { 0 };
-	info->ftLastWriteTime  = toFileTime( dir->modified() );
-	
-	setFileSize( info->nFileSizeHigh, info->nFileSizeLow, dir->filesize() );
-	
-	//TODO: For files: FILE_ATTRIBUTE_READONLY
-	
-	return STATUS_SUCCESS;
+	try{
+		auto dir = getRoot( file_info ).getFromPath( { filename } );
+		if( !dir )
+			return STATUS_OBJECT_NAME_NOT_FOUND;
+		
+		info->dwFileAttributes = dir->isDir() ? FILE_ATTRIBUTE_DIRECTORY : FILE_ATTRIBUTE_NORMAL;
+		
+		info->ftCreationTime   = toFileTime( dir->created() );
+		info->ftLastAccessTime = { 0 };
+		info->ftLastWriteTime  = toFileTime( dir->modified() );
+		
+		setFileSize( info->nFileSizeHigh, info->nFileSizeLow, dir->filesize() );
+		
+		//TODO: For files: FILE_ATTRIBUTE_READONLY
+		
+		return STATUS_SUCCESS;
+	}
+	catch( std::exception& e ){
+		std::cout << e.what() << std::endl;
+		return STATUS_UNSUCCESSFUL;
+	}
 }
 
 NTSTATUS DOKAN_CALLBACK FindFiles( LPCWSTR path, PFillFindData insert, PDOKAN_FILE_INFO file_info ){
-	auto dir = getRoot( file_info ).getFromPath( { path } );
-	if( !dir )
-		return STATUS_OBJECT_NAME_NOT_FOUND;
-	
-	for( uint64_t i=0; i<dir->children(); i++ ){
-		WIN32_FIND_DATA file = { 0 };
-		auto& child = (*dir)[i];
+	try{
+		auto dir = getRoot( file_info ).getFromPath( { path } );
+		if( !dir )
+			return STATUS_OBJECT_NAME_NOT_FOUND;
 		
-		//Set attributes
-		file.dwFileAttributes = child.isDir() ? FILE_ATTRIBUTE_DIRECTORY : FILE_ATTRIBUTE_NORMAL;
-		setFileSize( file.nFileSizeHigh, file.nFileSizeLow, child.filesize() );
-		file.ftCreationTime   = toFileTime( child.created() );
-		file.ftLastWriteTime  = toFileTime( child.modified() );
+		for( uint64_t i=0; i<dir->children(); i++ ){
+			WIN32_FIND_DATA file = { 0 };
+			auto& child = (*dir)[i];
+			
+			//Set attributes
+			file.dwFileAttributes = child.isDir() ? FILE_ATTRIBUTE_DIRECTORY : FILE_ATTRIBUTE_NORMAL;
+			setFileSize( file.nFileSizeHigh, file.nFileSizeLow, child.filesize() );
+			file.ftCreationTime   = toFileTime( child.created() );
+			file.ftLastWriteTime  = toFileTime( child.modified() );
+			
+			//Copy file name
+			auto name = child.name();
+			require( name.size() < MAX_PATH );
+			for( unsigned j=0; j<name.size(); j++ )
+				file.cFileName[j] = name[j];
+			
+			//Add file using callback
+			if( insert( &file, file_info ) != 0 )
+				return STATUS_INTERNAL_ERROR;
+		}
 		
-		//Copy file name
-		auto name = child.name();
-		require( name.size() < MAX_PATH );
-		for( unsigned j=0; j<name.size(); j++ )
-			file.cFileName[j] = name[j];
-		
-		//Add file using callback
-		if( insert( &file, file_info ) != 0 )
-			return STATUS_INTERNAL_ERROR;
+		return STATUS_SUCCESS;
 	}
-	
-	return STATUS_SUCCESS;
+	catch( std::exception& e ){
+		std::cout << e.what() << std::endl;
+		return STATUS_UNSUCCESSFUL;
+	}
 }
 
 void DOKAN_CALLBACK Cleanup( LPCWSTR filename, PDOKAN_FILE_INFO file_info ){
-	//Free handle if it exists
-	auto file = contextToHandle( file_info );
-	if( file ){
-		delete file;
-		file_info->Context = 0;
+	try{
+		//Free handle if it exists
+		auto file = contextToHandle( file_info );
+		if( file ){
+			delete file;
+			file_info->Context = 0;
+		}
+	}
+	catch( std::exception& e ){
+		std::cout << e.what() << std::endl;
 	}
 }
 
